81496de48921fcc11fa00dba85ad83d9c6c84d64
Rodrigo Costa <rodrigo.costa@gmail.com>
2024-02-11 01:22:25 -0300

    OO Stack

---
+++ m3/stack.py
@@ -0,0 +1,68 @@
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+class AddingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__sum = 0
+    
+    def push(self, val):
+        self.__sum += val
+        Stack.push(self, val)
+    
+    def pop(self):
+        val = Stack.pop(self)
+        self.__sum -= val
+        return val
+    
+    def get_sum(self):
+        return self.__sum 
+
+
+stack_object = Stack()
+
+stack_object.push(3)
+stack_object.push(2)
+stack_object.push(1)
+
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
+
+stack_object_1 = Stack()
+stack_object_2 = Stack()
+
+stack_object_1.push(3)
+stack_object_2.push(stack_object_1.pop())
+
+print(stack_object_2.pop())
+
+little_stack = Stack()
+another_stack = Stack()
+funny_stack = Stack()
+
+little_stack.push(1)
+another_stack.push(little_stack.pop() + 1)
+funny_stack.push(another_stack.pop() - 2)
+
+print(funny_stack.pop())
+
+stack_object = AddingStack()
+
+for i in range(5):
+    stack_object.push(i)
+print(stack_object.get_sum())
+
+for i in range(5):
+    print(stack_object.pop())
